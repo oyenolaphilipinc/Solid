d2bc70f64db0247e8fd5f6c8147841f2f4eac6ad
Philip <oyenolaphilip89@gmail.com>
2024-05-23 19:47:05 +0000

    pushing .env.local now

--- utlis/supabase/server.ts
+++ utlis/supabase/server.ts
@@ -26,12 +26,3 @@ export function createClient() {
     }
   )
 }
-
-export async function getUser() {
-  const supabase = createClient();
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data.user) {
-    return null;
-  }
-  return data.user;
-}
